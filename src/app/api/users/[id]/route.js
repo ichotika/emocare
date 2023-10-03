be77046e137f151176621396eb0bd008e228b031
ichotika <64461759+ichotika@users.noreply.github.com>
2023-10-03 10:50:31 -0700

    setup project monogodb

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,27 @@
+import connectMongoDB from "@/libs/mongodb";
+import Users from "@/models/Users";
+import { NextResponse } from "next/server";
+
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const { 
+    newUid: uid,
+    newIsEmployee: isEmployee,
+    newFirstName: firstName,
+    newLastName: lastName,
+    newEmail: email,
+    newPhone: phone,
+    newAge: age,
+    newOrganization: organization,
+    } = await request.json();
+  await connectMongoDB();
+  await Users.findByIdAndUpdate(id, {uid,isEmployee,firstName,lastName,email,phone,age,organization});
+  return NextResponse.json({ message: "User updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const user = await Users.findOne({ _id: id });
+  return NextResponse.json({ user }, { status: 200 });
+}
