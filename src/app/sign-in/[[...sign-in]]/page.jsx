a291e3cd02f794119416c75f54b5156c661f768d
anhnguyen <pnguyen63@mylangara.ca>
2023-10-13 15:35:41 -0700

    Styled signin and signup pages

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,11 @@
-import { SignIn } from "@clerk/nextjs";
+import {SignIn} from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn />;
+    return (
+        <div className="grid lg:grid-cols-3">
+            <div
+                className="hidden lg:block rounded-2xl col-span-2 bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1560249956-b3731ecf3153')]"></div>
+            <div className="col-start-3 col-end-3 -ml-16"><SignIn/></div>
+        </div>
+    );
 }
