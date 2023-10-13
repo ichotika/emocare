271a117664007bf6e7bf43555bc0e3a321a8fe12
anhnguyen <pnguyen63@mylangara.ca>
2023-10-13 16:16:04 -0700

    Updated CSS ordering for signup and signin pages

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,11 +1,12 @@
-import {SignUp} from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
         <div className="grid lg:grid-cols-3">
-            <div
-                className="hidden lg:block rounded-2xl col-span-2 bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1560249956-b3731ecf3153')]"></div>
-            <div className="col-start-3 col-end-3 -ml-16"><SignUp/></div>
+            <div className="col-span-2 hidden rounded-2xl bg-[url('https://images.unsplash.com/photo-1560249956-b3731ecf3153')] bg-cover bg-center lg:block"></div>
+            <div className="col-start-3 col-end-3 -ml-16">
+                <SignUp />
+            </div>
         </div>
     );
 }
