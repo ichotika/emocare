a824311364c237c333de5168b7bd7377236ffa74
anhnguyen <pnguyen63@mylangara.ca>
2023-10-29 16:49:18 -0700

    Hero Banner and page access changes

--- src/components/company/heroBanner.jsx
+++ src/components/company/heroBanner.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 export default function HeroBanner() {
     return (
         <header>
-            <div>
-            <h1>Revolutize your <strong className="text-blue-500">Business</strong> with a Focus on Employee <strong className="text-orange-500">Mental Health</strong></h1>
-            <p></p>
-        </div>
+            <div className="mx-auto w-450px flex flex-col items-center">
+            <h1 className="text-5xl text-center">Revolutize your <strong className="text-blue-500">Business</strong> with a Focus on Employee <strong className="text-orange-500">Mental Health</strong></h1>
+            <p className="text-center">Mental Welness Drives Employee Performance and Company Posperity</p>
+            <button className="bg-blue-700 text-white">Get Started Now</button>
+            </div>
         </header>
     )
 }
