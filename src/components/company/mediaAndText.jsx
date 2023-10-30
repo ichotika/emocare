4ed41ea7b855a5b823f49cdb29dea69d9add1697
Phan Anh Nguyen <a1842873@student.adelaide.edu.au>
2023-10-30 01:57:12 -0700

    Created mediaAndText.jsx component

--- src/components/company/mediaAndText.jsx
+++ src/components/company/mediaAndText.jsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 
-export default function MediaAndText({heading,body,src,textLeft}) {
-    return (
-        <article className={"grid grid-cols-2 gap-8"}>
-            {children[0]}
-            {children[1]}
-        </article>
-    );
+// Need to somehow dynamically import images
+export default function MediaAndText({heading, body, src, textLeft}) {
+    return (<article className={"grid grid-cols-2 gap-8 items-center"}>
+        {textLeft ? (<>
+            <div>
+                <h2 className={"text-blue-700 text-2xl"}>{heading}</h2>
+                <p>{body}</p>
+            </div>
+            <Image src={src} alt={heading} width="0" height="0" sizes="100vw" className="w-full h-auto"></Image>
+        </>) : (<>
+            <Image src={src} alt={heading} width="0" height="0" sizes="100vw" className="w-full h-auto"></Image>
+            <div>
+                <h2 className={"text-blue-700 text-2xl"}>{heading}</h2>
+                <p>{body}</p>
+            </div>
+        </>)}
+    </article>);
 }
