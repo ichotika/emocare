e8195cea984ebb23875be7d3fb9fce844a2dc75a
Phan Anh Nguyen <a1842873@student.adelaide.edu.au>
2023-10-30 00:40:12 -0700

    Fixed image imports for top and bottom nav

--- src/components/company/footerNav.jsx
+++ src/components/company/footerNav.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import emocareLogo from "@/public/assets/Wireframes/EmoCare_logo 1.svg";
 
 
 export default function Footer({routes}) {
 
     return (
         <footer className={"flex flex-col items-center gap-8"}>
-            <Image src="/public/assets/Wireframes/EmoCare_logo 1.svg" alt="Emocare Logo"/>
+            <Image height={50} src={emocareLogo} alt="Emocare Logo"/>
             <ul className={"flex flex-row gap-4"}>
-                {routes.map(route => <Link className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>)}
+                {routes.map((route,index) => <Link key={index} className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>)}
             </ul>
             <p>Emocare &copy; 2023. All rights reserved. </p>
         </footer>
--- src/components/company/topNav.jsx
+++ src/components/company/topNav.jsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import emocareLogo from "@/public/assets/Wireframes/EmoCare_logo 1.svg";
 
 
 export default function TopNav({routes}) {
 
     return (<nav className={"flex flex-row items-center justify-between"}>
-        <Image src="/public/assets/Wireframes/EmoCare_logo 1.svg" alt="Emocare Logo"/>
+        <Image src={emocareLogo} height={50} alt="Emocare Logo"/>
         <div className={"flex flex-row gap-8"}>
             <ul className={"flex flex-row gap-4"}>
-                {routes.map(route => <Link className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>)}
+                {routes.map((route,index) => <Link key={index} className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>)}
             </ul>
             <div className={"flex flex-row gap-2"}>
                 <Link href={"/sign-in"}>
