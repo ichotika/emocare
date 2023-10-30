ab383a8740637f1e62f444562d4c98a2185da778
Phan Anh Nguyen <a1842873@student.adelaide.edu.au>
2023-10-30 01:02:06 -0700

    Added feature image to assets

--- src/components/company/mediaAndText.jsx
+++ src/components/company/mediaAndText.jsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
 
-
-export default function MediaAndText() {
-    return {abcde};
+export default function MediaAndText({heading,body,src,textLeft}) {
+    return (
+        <article className={"grid grid-cols-2 gap-8"}>
+            {children[0]}
+            {children[1]}
+        </article>
+    );
 }
