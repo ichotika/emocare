206c05640f44d2858ab86a2cff588c5fc7bb759c
Phan Anh Nguyen <a1842873@student.adelaide.edu.au>
2023-10-30 01:57:01 -0700

    Updated heroBanner.jsx to use svg background

--- src/components/company/heroBanner.jsx
+++ src/components/company/heroBanner.jsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function HeroBanner() {
     return (
-        <header>
-            <div className="mx-auto w-450px flex flex-col items-center">
+        <header className={"bg-[url('/company_site/heroBackground.svg')] h-96 grid items-center justify-items-center"}>
+            <div className="w-1/2 flex flex-col items-center">
             <h1 className="text-5xl text-center">Revolutize your <strong className="text-blue-500">Business</strong> with a Focus on Employee <strong className="text-orange-500">Mental Health</strong></h1>
             <p className="text-center">Mental Welness Drives Employee Performance and Company Posperity</p>
             <button className="bg-blue-700 text-white">Get Started Now</button>
