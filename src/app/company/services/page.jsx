7c4a05bf4ab6073904b8e98c811869356394add5
anhnguyen <pnguyen63@mylangara.ca>
2023-11-02 18:18:57 -0700

    Services Page header

---
+++ src/app/company/services/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Page() {
+    return (
+        <>
+        <header className="flex flex-col items-center">
+            <h1 className="text-5xl text-center">How it <strong classname="text-blue-500">Works</strong></h1>
+            <p className="text-center">We believe in making mental health support accessible, effective, and tailored to the unique needs of organizations and employees. Our platform EmoCare seamlessly serves two essential user roles: organizations and employees.</p>
+        </header>
+        </>
+    )
+}
