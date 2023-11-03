40460e07f24242ed3d310704ae199a975b5d540a
anhnguyen <pnguyen63@mylangara.ca>
2023-11-02 19:18:49 -0700

    Bugfixing styling for sign-in and sign-up

--- src/components/sign-up/FinalSignUpPage.jsx
+++ src/components/sign-up/FinalSignUpPage.jsx
@@ -5,8 +5,8 @@ import {SignUp} from "@clerk/nextjs";
 // Redesign to take orgName from URL params
 export default function FinalSignUpPage({ orgName }) {
     return (
-        <div className="grid lg:grid-cols-3">
-            <div className="col-span-2 hidden rounded-2xl bg-[url('https://images.unsplash.com/photo-1560249956-b3731ecf3153')] bg-cover bg-center lg:block"></div>
+        <div className="grid grid-cols-3 sm:grid-cols-1">
+            <div className="col-span-2 rounded-2xl bg-[url('https://images.unsplash.com/photo-1560249956-b3731ecf3153')] bg-cover bg-center sm:hidden"></div>
             <div className="col-start-3 col-end-3 -ml-16">
                 <SignUp unsafeMetadata={{ organization: orgName }} />
             </div>
