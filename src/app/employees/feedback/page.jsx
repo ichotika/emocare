eb53dda104deabfcec03369936ff0c09e90b52f1
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-07 02:27:40 -0800

    Updated Feedback page

--- src/app/employees/feedback/page.jsx
+++ src/app/employees/feedback/page.jsx
@@ -1,25 +1,30 @@
 export default function Page() {
     return (
-        <div className={"w-full"}>
+        <div className={"flex flex-col items-center"}>
             <h1 className={"text-4xl"}>Feedback</h1>
-            <div>
+            <div className={"mx-auto"}>
                 <form action="" className={"flex flex-col gap-4"}>
                     <h2>
                         Anonymous Feedback
                     </h2>
                     <label htmlFor="category" className={"flex flex-col items-start"}>
                         Category
-                        <select name="category" id="" className={"bg-gray-200 p-2 shadow-xl w-full"}>
+                        <select name="category" id="" required className={"bg-gray-200 p-2 shadow-xl w-full"}>
                             <option value="" disabled selected>Choose category</option>
+                            <option value="depression">Depression</option>
+                            <option value="anxiety">Anxiety</option>
+                            <option value="burnout">Burnout</option>
                         </select>
                     </label>
                     <label htmlFor="comment" className={"flex flex-col items-start"}>
                         Comment
                         <textarea name="comment" id="" cols="120" rows="10"
-                                  placeholder={"Write your comment here"} className={"bg-gray-200 p-2 shadow-xl"}></textarea>
+                                  placeholder={"Write your comment here"} className={"bg-gray-200 p-2 shadow-xl"}
+                                  required></textarea>
                     </label>
                     <div className={"flex flex-row justify-end gap-4"}>
-                        <input type="reset" value="Cancel" className={"cursor-pointer px-4 py-2 rounded-xl shadow-md border border-slate-200"}/>
+                        <input type="reset" value="Cancel"
+                               className={"cursor-pointer px-4 py-2 rounded-xl shadow-md border border-slate-200"}/>
                         <input type="submit" value="Submit"
                                className={"cursor-pointer px-4 py-2 rounded-xl shadow-md bg-blue-700 text-white"}/>
                     </div>
