4fc93f69b2d56b142d0865594436b5625df766ce
hridyauk <133180993+hridyauk@users.noreply.github.com>
2023-11-09 00:26:29 -0800

    personality content update, education header tab
    personality fetching content
    education header tab
    employee dashboard responsive layout

--- src/components/employees/NoResultChart.jsx
+++ src/components/employees/NoResultChart.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 function NoResultChart({ mainTitle, link }) {
     return (
-        <div className="grid-row-2 grid gap-4 rounded-lg bg-white text-center text-lg">
+        <div className="p-2 grow flex flex-col sm:flex-row items-center justify-between gap-4 rounded-lg bg-white text-center text-lg">
             <h1 className="m-1 font-bold">{mainTitle}</h1>
             <div className="p-2">
-                <p className="pb-2 font-bold text-slate-400">No result</p>
+                <p className="pb-2 font-bold text-slate-400 sm:hidden">No result</p>
                 <button className="rounded-lg bg-blue-700 p-2 text-white hover:bg-blue-500">
                     <Link href={link}>Take Assessment</Link>
                 </button>
--- src/components/employees/PersonalityDetail.jsx
+++ src/components/employees/PersonalityDetail.jsx
@@ -1,38 +1,45 @@
 import Image from "next/image";
 import Profile from "@/public/assets/Wireframes/bell.svg";
 import Link from "next/link";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 function PersonalityDetail({ personality }) {
-    console.log(personality);
     return (
         <>
-            <div className="m-4 grid grid-cols-3 p-4">
-                <div className="flex items-start justify-center">
-                    {/* <Image src={Profile} width={100} height={100} alt="Picture" /> */}
-                    <div className="h-[10rem] w-[10rem] rounded bg-slate-400"></div>
-                </div>
-                <div className="col-span-2 px-8">
-                    <h1 className="pb-4 font-bold">
-                        Your personality is {personality.type}
-                    </h1>
-                    <p>{personality.longdesc}</p>
-                    <p className="pt-8">
-                        For more information, visit{" "}
-                        <Link
-                            href={`https://www.16personalities.com/${personality.type}-personality`}
-                            target={"_blank"}
-                        >
-                            16 personalities
-                        </Link>
-                        .
-                    </p>
-                    <div className="flex justify-end pt-6">
-                        <button className="rounded-lg bg-blue-700 p-2 text-white hover:bg-blue-500">
-                            <Link href={`/employees`}>
-                                Go back to dashboard
-                            </Link>
-                        </button>
+            <div className="p-4">
+                <div className="grid grid-cols-3 py-4">
+                    <div className="flex items-start justify-center">
+                        {/* <Image src={Profile} width={100} height={100} alt="Picture" /> */}
+                        <div className="h-[10rem] w-[10rem] rounded bg-slate-400"></div>
                     </div>
+                    <div className="col-span-2 px-8">
+                        <h1 className="pb-4 font-bold">
+                            Your personality is {personality.type}
+                        </h1>
+                        <p>{personality.longdesc}</p>
+
+                    </div>
+                </div>
+
+                <div>{documentToReactComponents(personality.description)}</div>
+
+                <p className="pt-8">
+                    For more information, visit{" "}
+                    <Link
+                        href={`https://www.16personalities.com/${personality.type}-personality`}
+                        target={"_blank"}
+                    >
+                        16 personalities
+                    </Link>
+                    .
+                </p>
+
+                <div className="flex justify-end pt-6">
+                    <button className="rounded-lg bg-blue-700 p-2 text-white hover:bg-blue-500">
+                        <Link href={`/employees`}>
+                            Go back to dashboard
+                        </Link>
+                    </button>
                 </div>
             </div>
         </>
