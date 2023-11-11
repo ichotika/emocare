70e2e42d10645040ce40b6c612dba72ee1557cc8
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-10 20:08:21 -0800

    Added logic for feedback submission route

--- src/app/api/submit_feedback/route.js
+++ src/app/api/submit_feedback/route.js
@@ -1,15 +1,77 @@
 import connectMongoDB from "@/libs/mongodb";
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import Feedback from "@/models/Feedback";
+import {currentUser, auth} from "@clerk/nextjs";
 
 
-export async function GET (request) {
-    return NextResponse.json({message: "This route is up and operational"});
+export async function GET () {
+    const { userId } = auth();
+
+    if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const user = await currentUser();
+
+    console.log(user);
+    if (user.unsafeMetadata.role==="employee") {
+        return new NextResponse("Unauthorized. User not an employer", {status: 401});
+    }
+
+    try{
+        await connectMongoDB();
+        const feedbacks = await Feedback.find({organization_name:user.unsafeMetadata.organization});
+        console.log(feedbacks);
+        return NextResponse.json({feedbacks},{status: 200});
+    }catch(error){
+        console.error("Error fetching your organization's feedbacks: ", error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
 }
 
+// title is designation in Clerk user
+// description is content in POST request
 export async function POST (request){
-    const payload= await request.text();
-    console.log(payload);
-    return NextResponse.json({payload});
+    const { userId } = auth();
+
+    if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const user = await currentUser();
+
+    console.log(user);
+    if (user.unsafeMetadata.role!=="employee") {
+        return new NextResponse("Unauthorized. User not an employee", {status: 401});
+    }
+
+    await connectMongoDB();
+    try {
+        const formData= await request.formData();
+        const data = Object.fromEntries(formData);
+        console.log(data);
+        const {category,comment} = data;
+        const newFeedback = new Feedback({
+            clerk_id: user.id,
+            organization_name: user.unsafeMetadata.organization,
+            department: user.unsafeMetadata.department,
+            title: user.unsafeMetadata.designation,
+            category: category,
+            description: comment,
+        })
+
+        await newFeedback.save();
+        const dataResponse = newFeedback.toObject();
+
+        return NextResponse.json({message:"Feedback successfully submitted",data: dataResponse},{status: 201});
+    } catch(error) {
+        console.error("Error creating assessment: ", error);
+        return NextResponse.json(
+            { error: "Internal server error"},
+            {status: 500}
+        );
+    }
 }
