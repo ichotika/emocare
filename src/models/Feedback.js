d59eaea76bee5081d0f8e0da3d020db3eb8df81a
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-10 20:08:02 -0800

    Added Mongoose model for feedbacks collection

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -1,13 +1,20 @@
 import mongoose, {Schema} from "mongoose";
 
+
+
+// title is designation in Clerk user
+// description is content in POST request
 const FeedbackSchema = new Schema (
     {
         clerk_id: String,
         organization_name: String,
         department: String,
-        designation: String,
+        title: String,
         category: String,
-        content: String,
+        description: String,
+    },
+    {
+            timestamps: true,
     }
 )
 
