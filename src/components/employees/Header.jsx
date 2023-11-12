28d6d9d0e0b0ee39e3718f041ff65edbda35c30b
ChronosZen <krisana9852@gmail.com>
2023-11-12 15:10:27 -0800

    Move notification to component

--- src/components/employees/Header.jsx
+++ src/components/employees/Header.jsx
@@ -3,24 +3,40 @@
 "use client";
 import NotificationEmployee from "@/components/base/NotificationEmployee";
 import { useEffect, useState } from "react";
-const Header = ({ headertext, user }) => {
-    const [notification, setNotification] = useState("");
+import { useUser } from "@clerk/nextjs";
+const Header = ({ headertext }) => {
+    const [notification, setNotification] = useState([]);
+    const { user, isLoaded } = useUser();
+
     useEffect(() => {
         async function updateUser() {
-            const notiData = await fetchNoti();
-
-            const filteredNoti = await notiData.filter(
-                (noti) => user === noti.userid && noti.isRead === false
-            );
-            setNotification(filteredNoti);
+            if (user) {
+                try {
+                    const notiData = await fetchNoti();
+                    if (notiData && Array.isArray(notiData)) {
+                        const filteredNoti = notiData.filter(
+                            (noti) =>
+                                user.id === noti.userid && noti.isRead === false
+                        );
+                        setNotification(filteredNoti);
+                    }
+                } catch (error) {
+                    console.error("Error fetching notifications:", error);
+                }
+            }
         }
         updateUser();
     }, [user]);
+
     const fetchNoti = async () => {
         const res = await fetch("/api/notification/employee");
         const data = await res.json();
         return data.notiEmp;
     };
+    if (!isLoaded) {
+        return null;
+    }
+
     return (
         <header className="mb-12 mt-12 flex justify-between">
             <h1 className="text-5xl font-bold">{headertext}</h1>
