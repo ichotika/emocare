e0066f1eb3066c441293eb699e3b41cffad8193d
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2023-11-12 15:41:54 -0800

    style employees sidebar

--- src/components/base/EmployeeSidebar.jsx
+++ src/components/base/EmployeeSidebar.jsx
@@ -1,19 +1,49 @@
 "use client";
-import Logo from "@/public/icons/logo_white.svg";
+// import icons
+import MobileLogo from "@/public/icons/logo_mobile.svg";
+import DesktopLogo from "@/public/icons/logo_main.svg";
+// import Hamburger from "@/public/Wireframes/hamburgerMenu.svg";
+
+// import others 
 import Link from "next/link";
 import { SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
-const EmployeeSidebar = () => {
+import TopNav from "../company/topNav";
+
+const EmployeeSidebar = ({ menuRoutes, supportRoutes }) => {
+
     return (
         // display: flex, flex: space between
         <>
-            <div className="flex min-h-full flex-col justify-between bg-blue-700 text-white">
+            {/* <TopNav routes={routes}></TopNav> */}
+
                 <header className="p-5">
                     <Link href={"/"}>
-                        <Image src={Logo} alt="logo}" />
+                        <Image src={DesktopLogo} alt="Emocare-logo"
+                        className="fill-blue-700 text-blue-700 hover:fill-white hover:text-white pr-2" />
                     </Link>
+
                     <nav>
                         <ul>
+                            {menuRoutes.map((menu, index) =>
+                                <li 
+                                    key={index} 
+                                    className="rounded-lg px-2 py-3 hover:bg-blue-700 hover:text-white">
+                                    <Link 
+                                        className="side-menu flex"
+                                        href={`/employees/${menu.slug}`}
+                                    >
+                                        <Image
+                                            className="fill-blue-500" 
+                                            src={menu.image} 
+                                            alt={menu.name}
+                                            width={24} 
+                                            height={24} />
+                                        {menu.name}
+                                    </Link>
+                                </li>
+                            )}
+                            {/* 
                             <li className="rounded-lg px-2 py-3 hover:bg-blue-700 hover:text-white">
                                 <Link href="/employees">Dashboard</Link>
                             </li>
@@ -31,7 +61,7 @@ const EmployeeSidebar = () => {
                                 <Link href="/employees/emergency">
                                     Emergency Calling
                                 </Link>
-                            </li>
+                            </li> */}
                             {/* <Image src={Dashboard}  width={24} height={24} alt="dashboard icon" />
                                 <Image src={Assessment} width={24} height={24} alt="assessment icon" />
                                 <Image src={Education} width={24} height={24} alt="education icon" />
@@ -45,7 +75,6 @@ const EmployeeSidebar = () => {
                         <SignOutButton />
                     </div>
                 </footer>
-            </div>
         </>
     );
 };
