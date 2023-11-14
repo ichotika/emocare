62a875006dd2cc628742ec94720fad424bc09274
ChronosZen <krisana9852@gmail.com>
2023-11-13 17:10:10 -0800

    Restyling condition cards

--- src/components/organizations/AssessmentRecords.jsx
+++ src/components/organizations/AssessmentRecords.jsx
@@ -1,12 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import BadCondition from "@/public/assets/organization/badConditionIcon.svg";
-import GoodCondition from "@/public/assets/organization/goodConditionIcon.svg";
-import MedCondition from "@/public/assets/organization/medConditionIcon.svg";
+import BadCondition from "@/public/assets/organization/Decline.gif";
+import GoodCondition from "@/public/assets/organization/Growth.gif";
+import MedCondition from "@/public/assets/organization/Normal.gif";
 import RecordTable from "@/components/organizations/RecordTable";
 function AssessmentRecords({ emplist, prevObj, curObj }) {
-
     const compareObj = {
         difGood: (
             (curObj.good / curObj.total - prevObj.good / prevObj.total) *
@@ -23,56 +22,91 @@ function AssessmentRecords({ emplist, prevObj, curObj }) {
         ).toFixed(1),
     };
     return (
-        <div>
+        <>
             <div className="flex gap-5">
-                <div className="flex grow gap-5 rounded-xl border border-solid border-teal-600 bg-teal-50 p-7">
+                <div className="border-p-blue-5 flex grow gap-5 rounded-xl border border-solid p-7 shadow-md">
                     <div className="flex flex-col gap-1">
-                        <h3 className="block text-start text-2xl font-bold">
-                            {((curObj.good / curObj.total) * 100).toFixed(1)}%
-                        </h3>
-                        <p className="block text-start text-xl font-bold">
+                        <p className="block text-start text-b-lg font-semibold">
                             Good Condition
                         </p>
+                        <h3 className="block text-start text-h-2xl font-bold">
+                            {((curObj.good / curObj.total) * 100).toFixed(1)}%
+                        </h3>
+
                         <h3 className="block text-start text-sm font-semibold">
-                            {compareObj.difGood}%
+                            <span className="text-b-xs font-medium text-p-blue-1">
+                                {compareObj.difGood}%
+                            </span>
+                            <span className="ps-1 text-b-xs font-medium text-g-gray-1">
+                                vs last month
+                            </span>
                         </h3>
                     </div>
-                    <Image src={GoodCondition} alt="good condition" />
+                    <Image
+                        src={GoodCondition}
+                        alt="good condition"
+                        className="-rotate-90"
+                        height={100}
+                        width={100}
+                    />
                 </div>
-                <div className="flex grow gap-5 rounded-xl border border-solid border-yellow-400 bg-yellow-50 p-7">
+                <div className="border-p-blue-5 flex grow gap-5 rounded-xl border border-solid p-7 shadow-md">
                     <div className="flex flex-col gap-1">
-                        <h3 className="block text-start text-2xl font-bold">
+                        <p className="block text-start text-b-lg font-semibold">
+                            Moderate Condition
+                        </p>
+                        <h3 className="block text-start text-h-2xl font-bold">
                             {((curObj.decent / curObj.total) * 100).toFixed(1)}%
                         </h3>
-                        <p className="block text-start text-xl font-bold">
-                            Decent Condition
-                        </p>
+
                         <h3 className="block text-start text-sm font-semibold">
-                            {compareObj.difDecent}%
+                            <span className="text-b-xs font-medium text-p-blue-1">
+                                {compareObj.difDecent}%
+                            </span>
+                            <span className="ps-1 text-b-xs font-medium text-g-gray-1">
+                                vs last month
+                            </span>
                         </h3>
                     </div>
-                    <Image src={MedCondition} alt="decent condition" />
+                    <Image
+                        src={MedCondition}
+                        alt="decent condition"
+                        height={100}
+                        width={100}
+                    />
                 </div>
-                <div className="flex grow gap-5 rounded-xl border border-solid border-red-600 bg-red-50 p-7">
+                <div className="border-p-blue-5 flex grow gap-5 rounded-xl border border-solid p-7 shadow-md">
                     <div className="flex flex-col gap-1">
-                        <h3 className="block text-start text-2xl font-bold">
+                        <p className="block text-start text-b-lg font-semibold">
+                            Critical Condition
+                        </p>
+                        <h3 className="block text-start text-h-2xl font-bold">
                             {((curObj.critical / curObj.total) * 100).toFixed(
                                 1
                             )}
                             %
                         </h3>
-                        <p className="block text-start text-xl font-bold">
-                            Critical Condition
-                        </p>
+
                         <h3 className="block text-start text-sm font-semibold">
-                            {compareObj.difCritical}%
+                            <span className="text-b-xs font-medium text-p-blue-1">
+                                {compareObj.difCritical}%
+                            </span>
+                            <span className="ps-1 text-b-xs font-medium text-g-gray-1">
+                                vs last month
+                            </span>
                         </h3>
                     </div>
-                    <Image src={BadCondition} alt="critical condition" />
+                    <Image
+                        src={BadCondition}
+                        alt="critical condition"
+                        className="rotate-90"
+                        height={100}
+                        width={100}
+                    />
                 </div>
             </div>
             <RecordTable employeeList={emplist} />
-        </div>
+        </>
     );
 }
 
--- src/components/organizations/Header.jsx
+++ src/components/organizations/Header.jsx
@@ -2,8 +2,8 @@
 import Notification from "@/components/base/Notification";
 const Header = ({ headertext, notification, assessment }) => {
     return (
-        <div className="mb-12 mt-12 flex justify-between">
-            <h1 className="text-5xl font-bold">{headertext}</h1>
+        <div className="mb-6 flex justify-between">
+            <h1 className="text-h-3xl font-bold">{headertext}</h1>
             <Notification notification={notification} assessment={assessment} />
         </div>
     );
