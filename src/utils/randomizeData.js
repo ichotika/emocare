6ac3b4a5a2c10a4d6de5c84001e7bcfebab557a1
ChronosZen <krisana9852@gmail.com>
2023-11-14 11:29:49 -0800

    Update randomize data file

--- src/utils/randomizeData.js
+++ src/utils/randomizeData.js
@@ -1,14 +1,22 @@
 const fs = require("fs");
 
 const userIDs = [
+    "user_2Y0xrxG8MvcW2DCosLJimclY3Qy",
     "user_2XoieRxs3OMlyOhpJWrmuMt1ICc",
+    "user_2XmlKxMzrbWeCmYAykfKIc5AYO2",
+    "user_2XmZ48kI0zqFYIDVhGWcqWpcpDv",
+    "user_2XjJmTTENoeu4CZn29IvGtQVcaT",
+    "user_2Xj6laq466aLOP3JeMBLifmJT3q",
+    "user_2Xj6T57AGPoMOBXCT2LTQMf324s",
     "user_2Xj5N0npf6pJfGaHlIWLvjxBJSp",
     "user_2Xj10InQPYdmPjOR9VreThe9fic",
-    "user_2Xj6T57AGPoMOBXCT2LTQMf324s",
-    "user_2Xj6laq466aLOP3JeMBLifmJT3q",
-    "user_2XmZ48kI0zqFYIDVhGWcqWpcpDv",
-    "user_2XmlKxMzrbWeCmYAykfKIc5AYO2",
-    "user_2XoieRxs3OMlyOhpJWrmuMt1ICc",
+    "user_2XPEtkTKwMrPNxfwJI5Tlm7Lih5",
+    "user_2XPEPN06KiJLHjHhZtqObwybzLh",
+    "user_2Wm410lLaboT0TuFcqYiOgEjFBL",
+    "user_2Wm3vzaMwIEixd3yeUVZadaiEej",
+    "user_2Wm2YG63MkK5hmmnAjFbxFsgjsx",
+    "user_2Wm2BZttrdTbl64xxb2wnJsm9Cz",
+    "user_2WjIXHWCPMqRxSY3nl9oYdFhLJI",
 ];
 
 function getRandomFromArray(array) {
@@ -16,22 +24,34 @@ function getRandomFromArray(array) {
     return array.splice(index, 1)[0];
 }
 
-function getRandomScoreAndDescription() {
-    const ranges = [
-        { min: 0, max: 15, description: "Critical" },
-        { min: 16, max: 30, description: "Decent" },
-        { min: 31, max: 50, description: "Good" },
-    ];
+function getRandomScore(assessmentType) {
+    let max;
+    switch (assessmentType) {
+        case "depression":
+            max = 27;
+            break;
+        case "Anxiety":
+            max = 21;
+            break;
+        case "burnout":
+            max = 75;
+            break;
+    }
+    return Math.floor(Math.random() * (max + 1));
+}
 
-    const range = ranges[Math.floor(Math.random() * ranges.length)];
-    const score =
-        Math.floor(Math.random() * (range.max - range.min + 1)) + range.min;
-
-    return { score, description: range.description };
+function determineLevel(assessmentType, score) {
+    if (assessmentType === "depression") {
+        return score <= 4 ? "Good" : score <= 14 ? "Moderate" : "Critical";
+    } else if (assessmentType === "Anxiety") {
+        return score < 19 ? "Good" : score <= 49 ? "Moderate" : "Critical";
+    } else if (assessmentType === "burnout") {
+        return score < 19 ? "Good" : score <= 49 ? "Moderate" : "Critical";
+    }
 }
 
 function getRandomAssessmentType() {
-    const types = ["Depression", "Burn out", "Anxiety"];
+    const types = ["depression", "burnout", "Anxiety"];
     return types[Math.floor(Math.random() * types.length)];
 }
 
@@ -41,39 +61,27 @@ function getRandomDate(month) {
         .toString()
         .padStart(2, "0")}T12:00:00Z`;
 }
-function determineLevel(score) {
-    if (score <= 15) {
-        return "Critical";
-    } else if (score <= 30) {
-        return "Decent";
-    } else {
-        return "Good";
-    }
-}
-
 function generateData() {
     const data = [];
-    const months = ["09", "10", "11"];
+    const months = ["10", "11"];
 
     for (let month of months) {
-        let tempUserIDs = [...userIDs]; // Copy the userIDs for each month
+        let tempUserIDs = [...userIDs];
 
-        for (let i = 0; i < userIDs.length; i++) {
-            const { score, description } = getRandomScoreAndDescription();
-            const userID =
-                tempUserIDs.length > 0
-                    ? getRandomFromArray(tempUserIDs)
-                    : `RandomUser${i + 1}`;
-
-            const level = determineLevel(score);
+        while (tempUserIDs.length > 0) {
+            const userID = getRandomFromArray(tempUserIDs);
+            const assessmentType = getRandomAssessmentType();
+            const score = getRandomScore(assessmentType);
+            const level = determineLevel(assessmentType, score);
 
             data.push({
                 userId: userID,
-                assessmentType: getRandomAssessmentType(),
+                assessmentType: assessmentType,
                 score: score,
                 level: level,
-                levelDescription: description,
-                timestamp: getRandomDate(month), // Changed from 'timestamps' to 'timestamp'
+                levelDescription: level, // Same as level
+                createdAt: getRandomDate(month),
+                updatedAt: getRandomDate(month),
             });
         }
     }
