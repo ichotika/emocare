fe8e7340e31ac30bc182b982277dcf7133440bcc
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-14 16:56:31 -0800

    Reordered Tailwind for topNav.jsx

--- src/components/company/topNav.jsx
+++ src/components/company/topNav.jsx
@@ -1,13 +1,11 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import emocareLogo from "@/public/assets/Wireframes/EmoCare_logo 1.svg";
-import {useEffect, useState, useRef} from "react";
-import {usePathname} from "next/navigation";
+import { useEffect, useState, useRef } from "react";
+import { usePathname } from "next/navigation";
 
-
-export default function TopNav({routes}) {
-
+export default function TopNav({ routes }) {
     const pathname = usePathname();
 
     const [hiddenHamburgerNav, setHiddenHamburgerNav] = useState(true);
@@ -24,64 +22,88 @@ export default function TopNav({routes}) {
 
     const handleOpen = () => {
         setHiddenHamburgerNav(false);
-    }
+    };
 
     const handleClose = () => {
         setHiddenHamburgerNav(true);
-    }
+    };
 
     return (
         <>
-            <nav ref={topNavRef}
-                 className={"z-10 bg-white flex flex-row items-center justify-between sm:fixed sm:top-0 sm:right-0 sm:left-0"}>
-                <Image src={emocareLogo} height={50} alt="Emocare Logo"/>
-                <button onClick={handleOpen}
-                        className={"bg-blue-600 text-white text-3xl rounded-xl p-1 hidden sm:block"}>=
+            <nav
+                ref={topNavRef}
+                className={
+                    "z-10 flex flex-row items-center justify-between bg-white sm:fixed sm:left-0 sm:right-0 sm:top-0"
+                }
+            >
+                <Image src={emocareLogo} height={50} alt="Emocare Logo" />
+                <button
+                    onClick={handleOpen}
+                    className={
+                        "hidden rounded-xl bg-blue-600 p-1 text-3xl text-white sm:block"
+                    }
+                >
+                    =
                 </button>
                 <div className={"flex flex-row gap-8 sm:hidden"}>
                     <ul className={"flex flex-row gap-4"}>
-                        {routes.map((route, index) =>
+                        {routes.map((route, index) => (
                             <li key={index}>
-                                <Link className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>
-                            </li>)}
+                                <Link
+                                    className={"block"}
+                                    href={`/company/${route.slug}`}
+                                >
+                                    {route.name}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                     <div className={"flex flex-row gap-2"}>
-                        <Link href={"/sign-in"}>
-                            Member Login
-                        </Link>
-                        <Link href={"/company/contact"}>
-                            Request a Demo
-                        </Link>
+                        <Link href={"/sign-in"}>Member Login</Link>
+                        <Link href={"/company/contact"}>Request a Demo</Link>
                     </div>
                 </div>
             </nav>
-            <HamburgerNav spacing={topNavRef.current ? topNavRef.current.clientHeight : 0} className={"z-10"}
-                          routes={routes} tuckedAway={hiddenHamburgerNav} onClose={handleClose}></HamburgerNav>
+            <HamburgerNav
+                spacing={topNavRef.current ? topNavRef.current.clientHeight : 0}
+                className={"z-10"}
+                routes={routes}
+                tuckedAway={hiddenHamburgerNav}
+                onClose={handleClose}
+            ></HamburgerNav>
         </>
-
-    )
+    );
 }
 
-function HamburgerNav({routes, tuckedAway, onClose, spacing}) {
-
-
+function HamburgerNav({ routes, tuckedAway, onClose, spacing }) {
     return (
         <nav
-            className={`z-10 bg-blue-600 text-white flex flex-col gap-4 items-center fixed inset-0 top-[${spacing}px] transition-all ${tuckedAway ? "translate-x-full" : "translate-x-0"}`}>
+            className={`fixed inset-0 z-10 flex flex-col items-center gap-4 bg-blue-600 text-white top-[${spacing}px] transition-all ${
+                tuckedAway ? "translate-x-full" : "translate-x-0"
+            }`}
+        >
             <ul className={"flex flex-col gap-4"}>
-                {routes.map((route, index) =>
+                {routes.map((route, index) => (
                     <li key={index}>
-                        <Link className={"block"} href={`/company/${route.slug}`}>{route.name}</Link>
-                    </li>)}
+                        <Link
+                            className={"block"}
+                            href={`/company/${route.slug}`}
+                        >
+                            {route.name}
+                        </Link>
+                    </li>
+                ))}
             </ul>
             <div className={"flex flex-col gap-2"}>
-                <Link href={"/sign-in"}>
-                    Member Login
-                </Link>
-                <Link href={"/company/contact"}>
-                    Request a Demo
-                </Link>
+                <Link href={"/sign-in"}>Member Login</Link>
+                <Link href={"/company/contact"}>Request a Demo</Link>
             </div>
-            <button onClick={onClose} className={"bg-blue-600 text-white text-3xl rounded-full p-1"}>X</button>
-        </nav>)
+            <button
+                onClick={onClose}
+                className={"rounded-full bg-blue-600 p-1 text-3xl text-white"}
+            >
+                X
+            </button>
+        </nav>
+    );
 }
