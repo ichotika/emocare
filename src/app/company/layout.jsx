8c1e197b19fda5c63c47232294cc51fda1cb8264
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-15 17:24:25 -0800

    Added top margin for main content to go under the mobile nav bar

--- src/app/company/layout.jsx
+++ src/app/company/layout.jsx
@@ -1,39 +1,38 @@
 import TopNav from "@/components/company/topNav";
 import Footer from "@/components/company/footerNav";
 
-
 const routesTop = [
     {
         slug: "team",
-        name: "Our team"
+        name: "Our team",
     },
     {
         slug: "pricing",
-        name: "Pricing"
+        name: "Pricing",
     },
-]
+];
 
 const routesBottom = [
     {
         slug: "pricing",
-        name: "Pricing"
+        name: "Pricing",
     },
     {
         slug: "team",
-        name: "Meet Our team"
+        name: "Meet Our team",
     },
     {
         slug: "contact",
-        name: "Contact"
+        name: "Contact",
     },
-]
+];
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     return (
         <>
             <TopNav routes={routesTop}></TopNav>
-            {children}
+            <div className={"sm:mt-[50px]"}> {children}</div>
             <Footer routes={routesBottom}></Footer>
         </>
-    )
+    );
 }
