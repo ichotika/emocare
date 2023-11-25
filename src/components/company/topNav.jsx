ca5f8d3b2dab6724e6b1ddca46079478b20dd57a
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-24 17:53:44 -0800

    Top Navigation styling

--- src/components/company/topNav.jsx
+++ src/components/company/topNav.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import DesktopLogo from "@/public/icons/logo_main.svg";
+import Hamburger from "@/public/assets/Wireframes/hamburgerMenu.svg";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
@@ -13,6 +14,10 @@ export default function TopNav({ routes }) {
     useEffect(() => {
         handleClose();
     }, [pathname]);
+
+    const handleToggle = () => {
+        setHiddenHamburgerNav(!hiddenHamburgerNav);
+    };
 
     const handleOpen = () => {
         setHiddenHamburgerNav(false);
@@ -26,19 +31,18 @@ export default function TopNav({ routes }) {
         <>
             <nav
                 className={
-                    "z-10 flex h-20 flex-row items-center justify-between bg-white px-20 py-4 shadow-[0_2px_6px_0_rgba(0,0,0,0.15)] lg:fixed lg:left-0 lg:right-0 lg:top-0 lg:h-16"
+                    "z-20 flex h-20 flex-row items-center justify-between bg-white px-20 py-4 shadow-[0_2px_6px_0_rgba(0,0,0,0.15)] lg:fixed lg:left-0 lg:right-0 lg:top-0 lg:h-16"
                 }
             >
                 <Link href={"/company"}>
                     <Image src={DesktopLogo} alt="Emocare Logo" height={60} />
                 </Link>
-                <button
-                    onClick={handleOpen}
-                    className={
-                        "hidden rounded-xl bg-blue-600 p-1 text-3xl text-white lg:block"
-                    }
-                >
-                    =
+                <button onClick={handleToggle} className={"hidden lg:block"}>
+                    <Image
+                        src={Hamburger}
+                        alt="hamburger menu"
+                        className={"invert"}
+                    />
                 </button>
                 <div className={"flex flex-row gap-6 lg:hidden"}>
                     <ul className={"flex flex-row items-center gap-4"}>
@@ -88,17 +92,23 @@ export default function TopNav({ routes }) {
 }
 
 function HamburgerNav({ routes, tuckedAway, onClose }) {
+    const pathname = usePathname();
+
     return (
         <nav
-            className={`fixed inset-0 top-16 z-10 flex flex-col items-center gap-4 bg-blue-600 text-white transition-all ${
-                tuckedAway ? "translate-x-full" : "translate-x-0"
+            className={`fixed left-0 right-0 top-16 z-10 hidden flex-col items-center gap-5 bg-g-white-1 py-8 transition-all lg:flex shadow-[0_2px_6px_0_rgba(0,0,0,0.15)] ${
+                tuckedAway ? "-translate-y-full" : "translate-y-0"
             }`}
         >
-            <ul className={"flex flex-col gap-4"}>
+            <ul className={"flex flex-col items-center gap-4"}>
                 {routes.map((route, index) => (
                     <li key={index}>
                         <Link
-                            className={"block"}
+                            className={`block px-2 py-3 font-bold ${
+                                pathname === `/company/${route.slug}`
+                                    ? "text-p-blue-1 underline decoration-2 underline-offset-[12px]"
+                                    : ""
+                            }`}
                             href={`/company/${route.slug}`}
                         >
                             {route.name}
@@ -106,16 +116,30 @@ function HamburgerNav({ routes, tuckedAway, onClose }) {
                     </li>
                 ))}
             </ul>
-            <div className={"flex flex-col gap-2"}>
-                <Link href={"/sign-in"}>Member Login</Link>
-                <Link href={"/company/contact"}>Request a Demo</Link>
+            <div className={"flex flex-col gap-4"}>
+                <Link
+                    href={"/sign-in"}
+                    className={
+                        "rounded-lg border-2 border-p-blue-1 px-7 py-3 font-bold text-p-blue-1"
+                    }
+                >
+                    Member Login
+                </Link>
+                <Link
+                    href={"/company/contact"}
+                    className={
+                        "rounded-lg border-2 border-p-blue-1 bg-p-blue-1 px-7 py-3 font-bold text-g-white-1"
+                    }
+                >
+                    Request a Demo
+                </Link>
             </div>
-            <button
-                onClick={onClose}
-                className={"rounded-full bg-blue-600 p-1 text-3xl text-white"}
-            >
-                X
-            </button>
+            {/*<button*/}
+            {/*    onClick={onClose}*/}
+            {/*    className={"rounded-full bg-blue-600 p-1 text-3xl text-white"}*/}
+            {/*>*/}
+            {/*    X*/}
+            {/*</button>*/}
         </nav>
     );
 }
