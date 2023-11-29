13178afc3e7a21d8129a643d252153a5f42e9174
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-11-28 19:50:13 -0800

    Remove unused imports

--- src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
@@ -1,6 +1,4 @@
 import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
-import Bg from "@/public/assets/loginCard.png";
 export default function Page() {
     return (
         <SignIn />
