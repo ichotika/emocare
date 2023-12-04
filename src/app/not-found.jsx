78cce419d1c0d013d2437b3e12744c5064b58d20
ChronosZen <krisana9852@gmail.com>
2023-12-03 16:19:52 -0800

    update 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,22 +2,25 @@
 import Link from "next/link";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
+import Image from "next/image";
+import ErrScreen from "@/public/assets/404.svg";
 const NotFound = () => {
     const router = useRouter();
 
     useEffect(() => {
         setTimeout(() => {
-            router.push("/");
-        }, 4000);
+            router.push("/company");
+        }, 2500);
     }, [router]);
 
     return (
-        <div className="not-found">
-            <h1>404</h1>
-            <h2>Oops! That page cannot be found :(</h2>
-            <p>
-                Redirecting to <Link href="/">Homepage</Link>
+        <div className="not-found flex h-screen w-screen flex-col items-center justify-center gap-2">
+            <Image src={ErrScreen} alt="404" />
+            <p className="text-b-2xl">
+                Redirecting to{" "}
+                <Link href="/" className="text-p-blue-1">
+                    Homepage
+                </Link>
             </p>
         </div>
     );
