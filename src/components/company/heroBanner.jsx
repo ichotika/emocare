236b008a7ece284ff4dc587e8886d060c517d29e
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-12-05 14:43:09 -0800

    Add linebreaks to heroBanner.jsx

--- src/components/company/heroBanner.jsx
+++ src/components/company/heroBanner.jsx
@@ -7,12 +7,10 @@ export default function HeroBanner() {
                 "grid items-center justify-items-center bg-[url('/company_site/heroBackground.svg')] bg-cover py-48 sm:py-20"
             }
         >
-            <div className="flex w-1/2 flex-col items-center gap-4 px-10 sm:w-full">
+            <div className="flex flex-col items-center gap-4 px-10 sm:w-full">
                 <h1 className="text-center text-6xl font-semibold sm:text-h-xl">
-                    Revolutionize your{" "}
-                    <span className="text-p-blue-1">Business</span> with a Focus
-                    on Employee{" "}
-                    <span className="text-s-orange-1">Mental Health</span>
+                    Revolutionize your <span className="text-p-blue-1">Business</span><br/>with a Focus
+                    on Employee<br/><span className="text-s-orange-1">Mental Health</span>
                 </h1>
                 <p className="text-center text-b-lg">
                     Mental Wellness Drives Employee Performance and Company
