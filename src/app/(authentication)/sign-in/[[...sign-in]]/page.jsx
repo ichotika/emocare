f65719dfaa0e31a7e2d27e1cc8cff874fef6c75d
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-12-05 19:26:31 -0800

    Styling update for the test account note

--- src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
@@ -6,15 +6,17 @@ export default function Page() {
             <SignIn initialValues={{emailAddress: "krisanaforico+99@gmail.com"}}/>
             <article
                 className={
-                    "rounded-lg bg-p-blue-1 p-8 text-g-white-1 shadow-2xl font-manrope"
+                    "rounded-lg bg-p-blue-1 p-8 text-g-white-1 shadow-2xl font-manrope flex flex-col gap-5"
                 }
             >
                 <h2 className={"font-archivo text-xl font-bold"}>Test accounts:</h2>
-                <h3 className={"font-bold text-lg pl-6"}>Employer:</h3>
-                <p className={"pl-12"}>Username: <span className={"font-bold"}>pre-filled</span></p>
-                <p className={"pl-12"}>Password: <span className={"font-bold"}>sBDmzn4kjf5pmHW</span></p>
-                <h2 className={"font-bold text-lg pl-6"}>Employee:</h2>
-                <p className={"pl-12"}>Sign up with &quot;WMDD Organization&quot;</p>
+                <div>                <h3 className={"font-bold text-lg pl-6"}>Employer:</h3>
+                    <p className={"pl-12"}>Username: <span className={"font-bold"}>pre-filled</span></p>
+                    <p className={"pl-12"}>Password: <span className={"font-bold"}>sBDmzn4kjf5pmHW</span></p></div>
+
+                <div>                <h2 className={"font-bold text-lg pl-6"}>Employee:</h2>
+                    <p className={"pl-12"}>Sign up with &quot;WMDD Organization&quot;</p></div>
+
             </article>
         </div>
     );
