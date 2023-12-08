3da3e8ebcd979e9f72989e75ed246401ac51130d
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2023-12-07 22:46:44 -0800

    Add ClerkLoading and ClerkLoaded to signin

--- src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(authentication)/sign-in/[[...sign-in]]/page.jsx
@@ -1,23 +1,31 @@
-import { SignIn } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading ,SignIn } from "@clerk/nextjs";
+import LoadingGif from "@/components/base/Loading";
 
 export default function Page() {
     return (
-        <div className={"flex flex-col gap-y-10"}>
-            <SignIn initialValues={{emailAddress: "krisanaforico+99@gmail.com"}}/>
-            <article
-                className={
-                    "rounded-lg bg-p-blue-1 p-8 text-g-white-1 shadow-2xl font-manrope flex flex-col gap-5"
-                }
-            >
-                <h2 className={"font-archivo text-xl font-bold"}>Test accounts:</h2>
-                <div>                <h3 className={"font-bold text-lg pl-6"}>Employer:</h3>
-                    <p className={"pl-12"}>Username: <span className={"font-bold"}>pre-filled</span></p>
-                    <p className={"pl-12"}>Password: <span className={"font-bold"}>sBDmzn4kjf5pmHW</span></p></div>
+        <>
+            <ClerkLoading>
+                <LoadingGif />
+            </ClerkLoading>
+            <ClerkLoaded>
+                <div className={"flex flex-col gap-y-10"}>
+                    <SignIn initialValues={{emailAddress: "krisanaforico+99@gmail.com"}}/>
+                    <article
+                        className={
+                            "rounded-lg bg-p-blue-1 p-8 text-g-white-1 shadow-2xl font-manrope flex flex-col gap-5"
+                        }
+                    >
+                        <h2 className={"font-archivo text-xl font-bold"}>Test accounts:</h2>
+                        <div>                <h3 className={"font-bold text-lg pl-6"}>Employer:</h3>
+                            <p className={"pl-12"}>Username: <span className={"font-bold"}>pre-filled</span></p>
+                            <p className={"pl-12"}>Password: <span className={"font-bold"}>sBDmzn4kjf5pmHW</span></p></div>
 
-                <div>                <h2 className={"font-bold text-lg pl-6"}>Employee:</h2>
-                    <p className={"pl-12"}>Sign up with &quot;WMDD Organization&quot;</p></div>
+                        <div>                <h2 className={"font-bold text-lg pl-6"}>Employee:</h2>
+                            <p className={"pl-12"}>Sign up with &quot;WMDD Organization&quot;</p></div>
 
-            </article>
-        </div>
+                    </article>
+                </div>
+            </ClerkLoaded>
+        </>
     );
 }
